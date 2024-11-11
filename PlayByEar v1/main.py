15531969e99deb588fee927c7eac67e208c7afae
sathviksimhap <sathviksimhap@gmail.com>
2024-11-11 20:55:13 +0530

    First

---
+++ PlayByEar v1/main.py
@@ -0,0 +1,16 @@
+from winsound import Beep
+
+# Happy Birthday BB
+Beep(392, 250)
+Beep(392, 250)
+Beep(440, 500)
+Beep(392, 500)
+Beep(523, 500)
+Beep(493, 1000)
+
+Beep(392, 250)
+Beep(392, 250)
+Beep(440, 500)
+Beep(392, 500)
+Beep(587, 500)
+Beep(523, 1000)
