9b6951beac9f3e0b96281dc36bf5c5278686f718
sathviksimhap <sathviksimhap@gmail.com>
2025-02-13 11:31:29 +0530

    minimum viable product ready

--- PlayByEar v1/functions.py
+++ PlayByEar v1/functions.py
@@ -1,11 +1,64 @@
-from notes import *
+from data import *
 from random import *
 from scamp import *
 
-def play_scale(key):
+def get_rand_key():
+    global notes_list
+    return choice(notes_list)
 
-    s = Session(tempo=120)
+def get_scale(key=get_rand_key(), mode = "easy"):
+    global notes_freq
+
+    start_scale = randint(0,1)
+    note = notes_freq[key][start_scale]
+
+    playable_notes = [note]
+    for i in range(modes[mode]):
+        for i in range(2):
+            note+=2
+            playable_notes.append(note)
+        note += 1
+        playable_notes.append(note)
+        for i in range(3):
+            note+=2
+            playable_notes.append(note)
+        note += 1
+        playable_notes.append(note)
+
+    new_scale = {
+        "key": key,
+        "octave": start_scale+1,
+        "notes": playable_notes
+    }
+
+    return new_scale
+
+def gen_question(scale, length = 3):
+    notes = scale["notes"]
+    questions = choices(notes, k = length)
+
+    answers = []
+    for note in questions:
+        answers.append(notes_list[note % 12])
+
+    question = {
+        "notes": questions,
+        "names": answers
+    }
+
+    return question
+
+def play_question(question):
+    notes = question["notes"]
+
+    s = Session()
     piano = s.new_part("piano")
+
+    for note in notes:
+        piano.play_note(note, 1, 1)
+
+
+
 
 
 
--- PlayByEar v1/main.py
+++ PlayByEar v1/main.py
@@ -1,4 +1,15 @@
 from scamp import *
-from notes import *
+from data import *
 from functions import *
 
+scale = get_scale()
+question = gen_question(scale, 5)
+
+while True:
+    n = input()
+    if n == "0":
+        break
+    if n == "2":
+        print(question["names"])
+    else:
+        play_question(question)
